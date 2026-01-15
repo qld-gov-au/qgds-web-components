1bb17ccb67094f99acb589e241f4ee37d41a0ff0
longhssq <long.huang@smartservice.qld.gov.au>
2026-01-15 23:59:38 +1000

    chore: style fix from feedbacks

--- src/types/qgds-components.d.ts
+++ src/types/qgds-components.d.ts
@@ -1,9 +1,11 @@
 // Purpose: Register all QDGS components in TypeScript's DOM API
 import type { QGDSCallout } from "../components/qgds-callout/qgds-callout";
+import type { QGDSInpageNav } from "../components/qgds-inpage-nav/qgds-inpage-nav";
 
 declare global {
   interface HTMLElementTagNameMap {
     "qgds-callout": QGDSCallout;
+    "qgds-inpage-nav": QGDSInpageNav;
   }
 }
 
