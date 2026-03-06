4d81e54768f4ca0a912638f29b6407053176ba4e
Steven Lynagh <steve.lynagh@smartservice.qld.gov.au>
2026-03-06 15:53:12 +1000

    chore:  lint

--- src/components/qgds-text-input/qgds-text-input.stories.ts
+++ src/components/qgds-text-input/qgds-text-input.stories.ts
@@ -1,7 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { getStorybookHelpers } from "@wc-toolkit/storybook-helpers";
-import { html } from "lit";
-
 import { QGDSTextInput } from "./qgds-text-input";
 import "./qgds-text-input";
 
@@ -28,44 +26,5 @@ export const Default: Story = {
     label: "Here is the label",
     hint: "This is hint text",
     placeholder: "Hold my place...",
-  },
-};
-
-export const Interaction: Story = {
-  args: { ...Default.args, "data-testid": "test" },
-  render: (args) => {
-    return html`
-      <style>
-        input:hover {
-          border-color: green;
-        }
-      </style>
-      <input type="text" value="my value" data-testid="native" />
-      <qgds-text-input
-        id=${args.id}
-        label=${args.label}
-        hint=${args.hint}
-        placeholder=${args.placeholder}
-        data-testid=${args["data-testid"]}
-      ></qgds-text-input>
-    `;
-  },
-  play: async ({ canvas, userEvent }) => {
-    const native = canvas.getByTestId("native");
-    const host = canvas.getByTestId("test");
-    const input = host?.shadowRoot?.querySelector("input");
-    console.log(host);
-    console.log(input);
-
-    if (input) {
-      await userEvent.tab();
-      await userEvent.click(host);
-      await userEvent.tab();
-      await userEvent.click(input);
-      await userEvent.hover(native);
-      await userEvent.input(input, "write me text");
-
-      // await userEvent.type(input, "I have a value!");
-    }
   },
 };
