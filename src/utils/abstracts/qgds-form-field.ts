ad7c75c91746f24318e390c6283aa98809fef335
longhssq <long.huang@smartservice.qld.gov.au>
2026-03-16 19:55:20 +1000

    chore: remove validationState from method params

--- src/utils/abstracts/qgds-form-field.ts
+++ src/utils/abstracts/qgds-form-field.ts
@@ -89,7 +89,7 @@ export abstract class QGDSFormField extends LitElement {
     return undefined;
   }
 
-  abstract renderInput(validationState?: "success" | "error"): TemplateResult;
+  abstract renderInput(): TemplateResult;
 
   render() {
     if (!this.id) {
@@ -112,6 +112,6 @@ export abstract class QGDSFormField extends LitElement {
           `
         : ""}
       <slot name="details"></slot>
-      ${this.renderInput(this.validationState)}`;
+      ${this.renderInput()}`;
   }
 }
