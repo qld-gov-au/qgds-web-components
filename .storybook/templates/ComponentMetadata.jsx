e1e109115c1a68fde17c97aa871d79e419936d4b
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2026-03-19 22:25:06 +1000

    docs(storybook): Add component tag to docs overview page

--- .storybook/templates/ComponentMetadata.jsx
+++ .storybook/templates/ComponentMetadata.jsx
@@ -5,7 +5,15 @@ import manifest from "../../custom-elements.json";
 
 const getComponentData = () => {
   const resolvedOf = useOf("meta", ["meta"]);
+
   const componentName = resolvedOf.preparedMeta?.component || "";
+
+  if (componentName === "") {
+    console.warn(
+      `Please add a component name to Storybook Meta config. See example at https://wc-toolkit.com/integrations/storybook/#setup`
+    );
+  }
+
   const component = getComponentByTagName(manifest, componentName);
 
   // Exit early
@@ -17,10 +25,9 @@ const getComponentData = () => {
   // Return subset of metadata relevant to the links we want to render.
   // Any custom @tags usually contain name and description keys, refer custom-elements.json for structure
   return {
+    tagname: component.tagName,
     uikit:
-      typeof component.uikit === "string"
-        ? component.uikit
-        : (component.uikit?.name ?? component.uikit?.description),
+      typeof component.uikit === "string" ? component.uikit : (component.uikit?.name ?? component.uikit?.description),
     website:
       typeof component.website === "string"
         ? component.website
@@ -29,8 +36,9 @@ const getComponentData = () => {
 };
 
 // Returns the JSX block <ComponentLinks /> for use on the custom DocumentationTemplate page
-export const ComponentLinks = () => {
+export const Links = () => {
   const metadata = getComponentData();
+  console.log("Component metadata for links:", metadata);
 
   // Check if some metadata exists
   if (!metadata || !Object.values(metadata).some(Boolean)) {
@@ -45,18 +53,44 @@ export const ComponentLinks = () => {
           marginBottom: "2rem",
           display: "flex",
           gap: "0.75rem",
-        }}>
+        }}
+      >
         {metadata.uikit && (
-          <a href={metadata.uikit} target='_blank' rel='noopener noreferrer'>
+          <a href={metadata.uikit} target="_blank" rel="noopener noreferrer">
             Figma UI Kit
           </a>
         )}
         {metadata.website && (
-          <a href={metadata.website} target='_blank' rel='noopener noreferrer'>
+          <a href={metadata.website} target="_blank" rel="noopener noreferrer">
             Guidelines
           </a>
         )}
       </p>
+    </>
+  );
+};
+
+export const TagName = () => {
+  const metadata = getComponentData();
+
+  if (!metadata?.tagname || metadata.tagname.trim() === "") {
+    return null;
+  }
+
+  return (
+    <>
+      <code
+        style={{
+          fontSize: "13px",
+          padding: "0.25rem",
+          border: "1px solid rgb(236, 242, 249)",
+          backgroundColor: "rgb(246, 249, 252)",
+          color: "#2e3338",
+          borderRadius: "3px",
+        }}
+      >
+        &lt;{metadata.tagname}&gt;
+      </code>
     </>
   );
 };
