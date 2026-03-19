fa367ea8b715a8e5df3c3f817b83355284f9431a
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2026-03-19 10:18:36 +1000

    build(index): Update barrel export file with new components

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,18 @@
 // =============================================================================
 export { QGDSButton } from "./components/qgds-button/qgds-button";
 export { QGDSCallout } from "./components/qgds-callout/qgds-callout";
+export { QGDSCheckbox } from "./components/qgds-checkbox/qgds-checkbox";
+export { QGDSCheckboxGroup } from "./components/qgds-checkbox-group/qgds-checkbox-group";
 export { QGDSDetails } from "./components/qgds-details/qgds-details";
 export { QGDSIcon } from "./components/qgds-icon/qgds-icon";
 export { QGDSInpageAlert } from "./components/qgds-inpage-alert/qgds-inpage-alert";
 export { QGDSInpageNav } from "./components/qgds-inpage-nav/qgds-inpage-nav";
-export { QGDSSelect } from "./components/qgds-select/qgds-select";
 export { QGDSInpageNavItem } from "./components/qgds-inpage-nav/qgds-inpage-nav-item";
+export { QGDSRadio } from "./components/qgds-radio/qgds-radio";
+export { QGDSRadioGroup } from "./components/qgds-radio-group/qgds-radio-group";
+export { QGDSSelect } from "./components/qgds-select/qgds-select";
+export { QGDSTextInput } from "./components/qgds-text-input/qgds-text-input";
+export { QGDSTextarea } from "./components/qgds-textarea/qgds-textarea";
 
 // etc...
 
