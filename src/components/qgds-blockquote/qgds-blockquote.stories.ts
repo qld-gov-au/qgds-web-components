3117b1417a8bbef538691b1c653d43baaecd290a
Teena Francis <Teena.Francis@smartservice.qld.gov.au>
2026-04-14 15:42:50 +1000

    feat: added stories for each palette color

--- src/components/qgds-blockquote/qgds-blockquote.stories.ts
+++ src/components/qgds-blockquote/qgds-blockquote.stories.ts
@@ -14,10 +14,7 @@ const meta: Meta<Args> = {
   component: "qgds-blockquote",
   tags: ["autodocs"],
   args,
-  argTypes: {
-    ...argTypes,
-  },
-
+  argTypes,
   render: (args) => template(args),
 };
 export default meta;
@@ -29,4 +26,44 @@ export const Default: Story = {
     "cite-label": "Sir Tim Berners-Lee",
     "cite-url": "https://example.com/source",
   },
+};
+
+export const Soft: Story = {
+  args: {
+    "default-slot":
+      "<p>The goal of the Web is to serve humanity. We build it now so that those who come to it later will be able to create things we cannot ourselves imagine.</p>",
+    "cite-label": "Sir Tim Berners-Lee",
+    "cite-url": "https://example.com/source",
+  },
+  globals: { globalPalette: "soft" },
+};
+
+export const Muted: Story = {
+  args: {
+    "default-slot":
+      "<p>The goal of the Web is to serve humanity. We build it now so that those who come to it later will be able to create things we cannot ourselves imagine.</p>",
+    "cite-label": "Sir Tim Berners-Lee",
+    "cite-url": "https://example.com/source",
+  },
+  globals: { globalPalette: "muted" },
+};
+
+export const Bold: Story = {
+  args: {
+    "default-slot":
+      "<p>The goal of the Web is to serve humanity. We build it now so that those who come to it later will be able to create things we cannot ourselves imagine.</p>",
+    "cite-label": "Sir Tim Berners-Lee",
+    "cite-url": "https://example.com/source",
+  },
+  globals: { globalPalette: "bold" },
+};
+
+export const Deep: Story = {
+  args: {
+    "default-slot":
+      "<p>The goal of the Web is to serve humanity. We build it now so that those who come to it later will be able to create things we cannot ourselves imagine.</p>",
+    "cite-label": "Sir Tim Berners-Lee",
+    "cite-url": "https://example.com/source",
+  },
+  globals: { globalPalette: "deep" },
 };
