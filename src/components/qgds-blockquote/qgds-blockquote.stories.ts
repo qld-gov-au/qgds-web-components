82346dac419c190d1f52d54413f83b658dee3b37
Teena Francis <Teena.Francis@smartservice.qld.gov.au>
2026-04-15 13:18:01 +1000

    feat: Added chromatic config to test default story in all different modes

--- src/components/qgds-blockquote/qgds-blockquote.stories.ts
+++ src/components/qgds-blockquote/qgds-blockquote.stories.ts
@@ -26,44 +26,16 @@ export const Default: Story = {
     "cite-label": "Sir Tim Berners-Lee",
     "cite-url": "https://example.com/source",
   },
-};
-
-export const Soft: Story = {
-  args: {
-    "default-slot":
-      "<p>The goal of the Web is to serve humanity. We build it now so that those who come to it later will be able to create things we cannot ourselves imagine.</p>",
-    "cite-label": "Sir Tim Berners-Lee",
-    "cite-url": "https://example.com/source",
+  parameters: {
+    chromatic: {
+      //🔶 Test default story in all different modes
+      modes: {
+        default: { globals: { globalPalette: "default" } },
+        soft: { globals: { globalPalette: "soft" } },
+        muted: { globals: { globalPalette: "muted" } },
+        bold: { globals: { globalPalette: "bold" } },
+        deep: { globals: { globalPalette: "deep" } },
+      },
+    },
   },
-  globals: { globalPalette: "soft" },
-};
-
-export const Muted: Story = {
-  args: {
-    "default-slot":
-      "<p>The goal of the Web is to serve humanity. We build it now so that those who come to it later will be able to create things we cannot ourselves imagine.</p>",
-    "cite-label": "Sir Tim Berners-Lee",
-    "cite-url": "https://example.com/source",
-  },
-  globals: { globalPalette: "muted" },
-};
-
-export const Bold: Story = {
-  args: {
-    "default-slot":
-      "<p>The goal of the Web is to serve humanity. We build it now so that those who come to it later will be able to create things we cannot ourselves imagine.</p>",
-    "cite-label": "Sir Tim Berners-Lee",
-    "cite-url": "https://example.com/source",
-  },
-  globals: { globalPalette: "bold" },
-};
-
-export const Deep: Story = {
-  args: {
-    "default-slot":
-      "<p>The goal of the Web is to serve humanity. We build it now so that those who come to it later will be able to create things we cannot ourselves imagine.</p>",
-    "cite-label": "Sir Tim Berners-Lee",
-    "cite-url": "https://example.com/source",
-  },
-  globals: { globalPalette: "deep" },
 };
