770a76ef7dae8cef2dbec38ce8e04fad176f794f
Steven Lynagh <steve.lynagh@smartservice.qld.gov.au>
2026-04-17 10:47:25 +1000

    fix: correct modes path

--- src/components/qgds-tag/qgds-tag.stories.ts
+++ src/components/qgds-tag/qgds-tag.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { getStorybookHelpers } from "@wc-toolkit/storybook-helpers";
 import { action } from "storybook/actions";
-import { chromaticModes } from "../../modes";
+import { chromaticModes } from "../../../.storybook/modes";
 import { html } from "lit";
 import type { QGDSTag } from "./qgds-tag";
 import "./qgds-tag";
