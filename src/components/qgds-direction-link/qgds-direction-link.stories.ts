5b63cc0d50ead10284f0f2ce905d1e3a48c968d3
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2026-05-19 09:27:59 +1000

    feat: add Back to Top story

--- src/components/qgds-direction-link/qgds-direction-link.stories.ts
+++ src/components/qgds-direction-link/qgds-direction-link.stories.ts
@@ -1,7 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { getStorybookHelpers } from "@wc-toolkit/storybook-helpers";
+import { html } from "lit";
 import type { QGDSDirectionLink } from "./qgds-direction-link.js";
 import "./qgds-direction-link.js";
+import { withEventAction } from "../../../.storybook/storybook-helpers";
 
 const { args, argTypes, template } = getStorybookHelpers<QGDSDirectionLink>("qgds-direction-link");
 
@@ -28,7 +30,27 @@ const meta: Meta<Args> = {
       control: "boolean",
     },
   },
+  decorators: [withEventAction("scroll-to-element")],
   render: (args) => template(args),
+};
+
+const scrollToElement = (id: string) => (event: Event) => {
+  event.preventDefault();
+
+  document.getElementById(id)?.scrollIntoView({
+    behavior: 'smooth',
+  });
+
+  event.currentTarget?.dispatchEvent(
+    new CustomEvent("scroll-to-element", {
+      detail: {
+        href: `#${id}`,
+        action: `scroll-to-${id}`,
+      },
+      bubbles: true,
+      composed: true,
+    })
+  );
 };
 
 export default meta;
@@ -38,4 +60,25 @@ export const Default: Story = {
   args: {
     direction: "right",
   },
+};
+
+export const BackToTop: Story = {
+  args: {
+    label: "Back to top",
+    href: "#top",
+    direction: "up",
+    animation: false,
+  },
+  render: (args) =>
+      html`
+        <h2 id="top" style="margin-block: 2rem;">Top of the page</h2>
+        <div style="margin-bottom: 100rem;">Scroll down to see the "Back to top" link.</div>
+        <qgds-direction-link
+          label=${args.label}
+          href=${args.href}
+          direction=${args.direction}
+          ?animation=${args.animation}
+          @click=${scrollToElement('top')}
+        ></qgds-direction-link>
+      `
 };
