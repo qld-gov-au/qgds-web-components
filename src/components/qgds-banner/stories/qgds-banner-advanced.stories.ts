82af682af60030f1cc2df276db11993b15688bff
Teena Francis <Teena.Francis@smartservice.qld.gov.au>
2026-06-16 15:51:36 +1000

    feat: Block title variant

--- src/components/qgds-banner/stories/qgds-banner-advanced.stories.ts
+++ src/components/qgds-banner/stories/qgds-banner-advanced.stories.ts
@@ -32,6 +32,8 @@ const meta: Meta<Args> = {
     "mobile-image-url": backgroundImageMobile,
     "image-description": "Background image",
     heading: "Lorem ipsum dolor sit amet, consectetur adipiscing sed",
+    "sub-heading": "Lorem ipsum dolor",
+    "is-block-type-heading": false,
     "background-option": "hero-image",
     "image-option": "grid-align",
     variant: "no-banner",
@@ -42,7 +44,53 @@ const meta: Meta<Args> = {
 
 export default meta;
 
-export const AdvancedStandardCTA: Story = {
+export const BlockTitleHeading: Story = {
+  args: {
+    heading: "Disaster recovery",
+    "sub-heading": "and support",
+    variant: "advanced",
+    "background-option": "none",
+    "is-block-type-heading": false,
+  },
+  argTypes: {
+    variant: {
+      control: false,
+      description:
+        "The variant of the banner, determines the layout and styling of the banner." +
+        "<br/>The variant `basic` used here is used to display a simplified banner layout which includes breadcrumbs, heading, abstract an an optional background texture or image.",
+    }, // Disables control
+    "background-option": {
+      control: false,
+      description: "The type of background to display in the banner. This variant uses the background option, `none`.",
+    }, // Disables control
+  },
+  parameters: {
+    controls: {
+      include: ["palette", "variant", "heading", "background-option", "sub-heading", "is-block-type-heading"],
+    }, // Shows ONLY specific ones
+  },
+  render: (args) => html`
+    <qgds-banner
+      palette=${args.palette}
+      variant=${args.variant}
+      heading=${args.heading}
+      sub-heading=${args["sub-heading"]}
+      background-option=${args["background-option"]}
+      .isBlockTypeHeading=${args["is-block-type-heading"]}
+    >
+      <qgds-breadcrumbs slot="breadcrumbs" aria-label="Breadcrumbs">
+        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/home">Home</qgds-breadcrumbs-item>
+        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level2">Level 2</qgds-breadcrumbs-item>
+        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level3">Level 3</qgds-breadcrumbs-item>
+        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level4">Level 4</qgds-breadcrumbs-item>
+        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/current_page">Current page</qgds-breadcrumbs-item>
+      </qgds-breadcrumbs>
+      <div>Renew your licence at a customer service centre, government office or police station.</div>
+    </qgds-banner>
+  `,
+};
+
+export const StandardCTA: Story = {
   args: meta.args,
   render: (args) => html`
     <qgds-banner
@@ -88,7 +136,7 @@ export const AdvancedStandardCTA: Story = {
   `,
 };
 
-export const AdvancedArrowCards: Story = {
+export const ArrowCards: Story = {
   args: meta.args,
   render: (args) => html`
     <qgds-banner
