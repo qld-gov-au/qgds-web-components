4bb50ef9307a73aecb684d562765eeb76ec8f1fb
Teena Francis <Teena.Francis@smartservice.qld.gov.au>
2026-06-16 13:39:36 +1000

    refactor: stories file separated for basic and advanced

--- src/components/qgds-banner/stories/qgds-banner-basic.stories.ts
+++ src/components/qgds-banner/stories/qgds-banner-basic.stories.ts
@@ -2,18 +2,17 @@ import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
 
 import { getStorybookHelpers } from "@wc-toolkit/storybook-helpers";
-import { chromaticModes } from "../../../.storybook/modes";
-import type { QGDSBanner } from "./qgds-banner";
-import { palettes } from "../../utils";
+import type { QGDSBanner } from "../qgds-banner";
+import { palettes } from "../../../utils";
 
-import "./qgds-banner";
-import "../qgds-breadcrumbs/qgds-breadcrumbs";
-import "../qgds-breadcrumbs/qgds-breadcrumbs-item";
-import "../qgds-button/qgds-button";
-import "../qgds-card/qgds-card";
-import backgroundImage from "../../img/banner-background-image.jpg";
-import backgroundImageMobile from "../../img/banner-background-image-mobile.jpg";
-import backgroundTexture from "../../img/banner-background-pattern.jpg";
+import "../qgds-banner";
+import "../../qgds-breadcrumbs/qgds-breadcrumbs";
+import "../../qgds-breadcrumbs/qgds-breadcrumbs-item";
+import "../../qgds-button/qgds-button";
+import "../../qgds-card/qgds-card";
+import backgroundImage from "../../../img/banner-background-image.jpg";
+import backgroundImageMobile from "../../../img/banner-background-image-mobile.jpg";
+import backgroundTexture from "../../../img/banner-background-pattern.jpg";
 
 const { args, argTypes, template } = getStorybookHelpers<QGDSBanner>("qgds-banner");
 
@@ -26,9 +25,8 @@ type Args = typeof args;
 type Story = StoryObj<Args>;
 
 const meta: Meta<Args> = {
-  title: "Components/Banner",
+  title: "Components/Banner/Basic",
   component: "qgds-banner",
-  tags: ["autodocs"],
   args: {
     ...args,
     "image-url": backgroundImage,
@@ -44,62 +42,6 @@ const meta: Meta<Args> = {
 };
 
 export default meta;
-
-export const NoBanner: Story = {
-  args: {
-    variant: "no-banner",
-  },
-  argTypes: {
-    variant: {
-      control: false,
-      description:
-        "The variant of the banner, determines the layout and styling of the banner. `no-banner` layout is used to display just breadcrumbs.",
-    }, // Disables control
-  },
-  parameters: {
-    controls: { include: ["palette", "variant"] }, // Shows ONLY specific ones
-  },
-  render: (args) => html`
-    <qgds-banner palette=${args.palette} variant=${args.variant}>
-      <qgds-breadcrumbs slot="breadcrumbs" aria-label="Breadcrumbs">
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/home">Home</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level2">Level 2</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level3">Level 3</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level4">Level 4</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/current_page">Current page</qgds-breadcrumbs-item>
-      </qgds-breadcrumbs>
-    </qgds-banner>
-  `,
-};
-
-export const DefaultBanner: Story = {
-  args: {
-    heading: "Lorem ipsum dolor sit amet, consectetur adipiscing sed",
-    variant: "default",
-    "background-option": "none",
-  },
-  argTypes: {
-    variant: {
-      control: false,
-      description:
-        "The variant of the banner, determines the layout and styling of the banner. `default` variant, is used to display breadcrumbs and heading.",
-    }, // Disables control
-  },
-  parameters: {
-    controls: { include: ["palette", "variant", "heading"] }, // Shows ONLY specific ones
-  },
-  render: (args) => html`
-    <qgds-banner palette=${args.palette} variant="default" heading=${args.heading}>
-      <qgds-breadcrumbs slot="breadcrumbs" aria-label="Breadcrumbs">
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/home">Home</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level2">Level 2</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level3">Level 3</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level4">Level 4</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/current_page">Current page</qgds-breadcrumbs-item>
-      </qgds-breadcrumbs>
-    </qgds-banner>
-  `,
-};
 
 export const BasicBannerNoBackground: Story = {
   args: {
@@ -226,92 +168,6 @@ export const BasicBannerWithBackgroundImage: Story = {
         <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/current_page">Current page</qgds-breadcrumbs-item>
       </qgds-breadcrumbs>
       <div>Renew your licence at a customer service centre, government office or police station.</div>
-    </qgds-banner>
-  `,
-};
-
-export const AdvancedStandardCTA: Story = {
-  args: meta.args,
-  parameters: {
-    ...chromaticModes,
-  },
-  render: (args) => html`
-    <qgds-banner
-      palette=${args.palette}
-      variant=${args.variant}
-      heading=${args.heading}
-      background-option=${args["background-option"]}
-      image-option=${args["image-option"]}
-      image-url=${args["image-url"]}
-      image-description=${args["image-description"]}
-    >
-      <qgds-breadcrumbs slot="breadcrumbs" aria-label="Breadcrumbs">
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/home">Home</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level2">Level 2</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level3">Level 3</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level4">Level 4</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/current_page">Current page</qgds-breadcrumbs-item>
-      </qgds-breadcrumbs>
-      <div>Renew your licence at a customer service centre, government office or police station.</div>
-      <div slot="cta">
-        <qgds-button
-          target="_self"
-          type="button"
-          aria-label="Primary Action"
-          label="Primary"
-          variant="primary"
-          href="https://www.qld.gov.au"
-          id=""
-        >
-        </qgds-button>
-        <qgds-button
-          target="_self"
-          type="button"
-          aria-label="Secondary Action"
-          label="Secondary"
-          variant="secondary"
-          href="https://www.qld.gov.au"
-          id=""
-        >
-        </qgds-button>
-      </div>
-    </qgds-banner>
-  `,
-};
-
-export const AdvancedArrowCards: Story = {
-  args: meta.args,
-  parameters: {
-    ...chromaticModes,
-  },
-  render: (args) => html`
-    <qgds-banner
-      palette=${args.palette}
-      heading=${args.heading}
-      background-option=${args["background-option"]}
-      image-option=${args["image-option"]}
-      image-url=${args["image-url"]}
-      image-description=${args["image-description"]}
-    >
-      <qgds-breadcrumbs slot="breadcrumbs" aria-label="Breadcrumbs">
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/home">Home</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level2">Level 2</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level3">Level 3</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/level4">Level 4</qgds-breadcrumbs-item>
-        <qgds-breadcrumbs-item target="_self" rel="bookmark" url="/current_page">Current page</qgds-breadcrumbs-item>
-      </qgds-breadcrumbs>
-      <div>Renew your licence at a customer service centre, government office or police station.</div>
-      <div slot="cards">
-        <qgds-card action="single" target="_blank" variant="arrow" heading="Card title" href="https://www.designsystem.qld.gov.au/components/card" palette="soft">
-        </qgds-card>
-    
-        <qgds-card action="single" target="_blank" variant="arrow" heading="Card title" href="https://www.designsystem.qld.gov.au/components/card" palette="soft">
-        </qgds-card>
-    
-        <qgds-card action="single" target="_blank" variant="arrow" heading="Card title" href="https://www.designsystem.qld.gov.au/components/card" palette="soft">
-        </qgds-card>
-        </qgds-button>
-      </div>
     </qgds-banner>
   `,
 };
