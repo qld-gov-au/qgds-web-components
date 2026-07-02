a0fc343d3e1c9331d35703fcb1a4bd6f8dae9fe5
Steve Pegg <steve@stevepegg.com>
2026-07-02 22:13:38 +1000

    fix: Remove size variant on icon - revert to LG default

--- src/components/qgds-loading-spinner/qgds-loading-spinner.ts
+++ src/components/qgds-loading-spinner/qgds-loading-spinner.ts
@@ -3,7 +3,6 @@ import { customElement, property } from "lit/decorators.js";
 import "../qgds-icon/qgds-icon";
 import styles from "./qgds-loading-spinner.styles.scss?inline";
 import { resetStyles, utilitiesStyles } from "../../styles";
-import type { IconSize } from "../qgds-icon/qgds-icon";
 
 /**
  * QGDS Loading Spinner
@@ -14,7 +13,6 @@ import type { IconSize } from "../qgds-icon/qgds-icon";
  * @uikit https://www.figma.com/design/qKsxl3ogIlBp7dafgxXuCA/QGDS-UI-kit?node-id=49314-16705
  * @website https://www.designsystem.qld.gov.au/components/loading-spinner
  *
- * @prop {IconSize} [size="md"] The size of the spinner icon: "xs", "sm", "md", "lg", "xl" or "xxl".
  * @prop {string} [label="Loading"] Text used as the accessible label. Also shown visually when labelVisible is true.
  * @prop {boolean} [hideLabel=false] Whether to display the label text visually below the spinner.
  * @prop {boolean} [isStacked=false] When true, stacks the icon and label vertically. When false, displays them inline horizontally.
@@ -23,9 +21,6 @@ import type { IconSize } from "../qgds-icon/qgds-icon";
 
 @customElement("qgds-loading-spinner")
 export class QGDSLoadingSpinner extends LitElement {
-  @property({ type: String, useDefault: true })
-  size: IconSize = "md";
-
   @property({ type: String, useDefault: true })
   label: string = "Loading";
 
@@ -40,7 +35,7 @@ export class QGDSLoadingSpinner extends LitElement {
   render() {
     return html`
       <div class="loading-spinner ${this.isStacked ? "is-stacked" : ""}" role="status" aria-label="${this.label}">
-        <qgds-icon class="spinner-icon" icon-id="spinner-step-1" size="${this.size}" aria-hidden="true"></qgds-icon>
+        <qgds-icon class="spinner-icon" icon-id="spinner-step-1" size="lg" aria-hidden="true"></qgds-icon>
 
         ${this.hideLabel
           ? html`<span class="sr-only">${this.label}</span>`
